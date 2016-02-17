3f20325af45f5c3c5d11ae71af39ef8b7b6766f6
shamshad <shamshad.npti@gmail.com>
2016-02-17 20:26:47 +0530

    Moved calc to old directory

---
+++ calc/js/site.js
@@ -0,0 +1,27 @@
+(function(){
+	$ = function($e){return document.getElementById($e)};
+	clear = function() { $("history").innerHTML = "";};
+	parser = math.parser();
+	append = function(e) {
+		if(!e || (e = e.trim()).length === 0) return;
+		if(e == "clear") {clear(); return false;}
+		t = $("history").innerHTML;
+		q = "<div class='question'>" + e + "</div>";
+		try {
+			s = "<div class='solution'>" + math.format(parser.eval(e), {precision: 6}) + "</div>";
+		} catch(err) {
+			s = "<div class='error'>" + err + "</div>";;
+		}
+		$("history").innerHTML = t + q + s;
+		$("history").scrollTop = $("history").scrollHeight;
+	}
+	$("calc").onsubmit = function(evt) {
+		evt.preventDefault();
+		append($("expression").value);
+		$("expression").value = "";
+		return false;
+	}
+	$("expression").focus();
+	$("expression").onblur = function() {$("expression").focus();};
+}());
+

7efd5ee270c7de521af10319664993841a0e28f9
shamshad <shamshad.npti@gmail.com>
2016-02-22 15:01:24 +0530

    Added licence to Solver

--- myblog/solver/Solver.java
+++ myblog/solver/Solver.java
@@ -1,7 +1,29 @@
-// java code for the intersection of implicit curve
+
+/** 
+ * Copyright 2016 Shamshad Alam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package myblog.solver;
 import java.util.List;
 import java.util.ArrayList;
+/** 
+ * Java code for the intersection of implicit curve
+ * This is a very naive implementation. You shouldn't 
+ * use this for any production purpose
+ */
 public class Solver {
 
     public static interface FunctionXY {
